6faf1ed261fde0db9024337798140b0a86589154
sasha sims <solely.sashasims@gmail.com>
2023-02-09 21:37:46 -0500

    challenge completed

--- main.js
+++ main.js
@@ -1,40 +1,107 @@
 
-class Player{
-    constructor(name, totalCoins, status, hasStar, setName){
-        this.name = this.name
+
+const statusArr = ["Powered Up", "Big", "Small", "Dead"]
+const statusUpdate = statusArr[Math.floor(Math.random() * statusArr.length)]
+
+
+class Player {
+    constructor(name, totalCoins, status, hasStar,) {
+        this.name = name
         this.totalCoins = totalCoins
-        this.status = this.status
-        this.hasStar = this.hasStar
-        this.setName = this.setName
-        this.gotHit = this.gotHit
-        this.gotPowerup = this.gotPowerup
-        this.addCoin = this.addCoin
-        this.print = this.print
+        this.status = status
+        this.hasStar = hasStar
+
     }
-    
-    setName(namePicked){
-this.name = namePicked
- return namePicked
-   
+
+
+    setName(namePicked) {
+        this.name = namePicked
+        return namePicked;
+
     }
-    
-    gotHit(){
-        
+
+    gotHit() {
+
+        this.status = statusUpdate
+        return statusUpdate
+
+
+
     }
-    gotPowerup(){
+
+
+
+
+
+    gotPowerup() {
+        console.log("you got a star!")
+        this.hasStar = true;
+        this.status = "Powered Up"
+        // this.status = statusUpdate
+        // return statusUpdate
+
     }
-    addCoin(){
-        player.totalCoins += 1
+    addCoin() {
+        player.totalCoins += 1;
+
     }
-    print(){
-        console.log({player})
+    print() {
+        console.log(player)
     }
+
 }
 
-// Don't repeat yourself . DRY
+
+const intervalId = setInterval(() => {
+    randomNum()
+}, 1000)
+function randomNum() {
+    
+    let num = Math.floor(Math.random() * 3)
+    if (num === 0) {
+        player.gotHit()
+    } else if (num === 1) {
+        player.gotPowerup()
+    } else {
+        player.addCoin()
+    }
+
+
+    player.print()
+  
+    player.status === "Dead"? stopPlaying() : intervalId
+
+
+}
+
+function stopPlaying() {
+    clearInterval(intervalId)
+  
+
+        console.log("game over! you died")
+
+    
+
+    }
+const player = new Player("", 0, "Small", false)
+player.setName("Mario")
+player.gotHit(statusUpdate)
+randomNum()
 
 
 
-const player = new Player("", 0, "small", true,    )
-console.log(player.setName("mario"))
-console.log(player.print())
+
+
+
+
+
+
+
+
+
+
+
+
+
+// player.gotPowerup()
+
